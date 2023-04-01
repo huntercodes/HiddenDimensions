d13022db798184af4ddecbb968a86d3b6b405404
hunter downey <85328038+huntercodes@users.noreply.github.com>
2023-03-31 22:40:02 -0500

    Initial commit
    Encryption/Decryption

---
+++ HiddenDimensions/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace HiddenDimensions
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtFileEncrypt = new System.Windows.Forms.TextBox();
+            this.txtEncryptPassword = new System.Windows.Forms.TextBox();
+            this.btnBrowseEncrypt = new System.Windows.Forms.Button();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.btnBrowseDecrypt = new System.Windows.Forms.Button();
+            this.txtDecryptPassword = new System.Windows.Forms.TextBox();
+            this.txtFileDecrypt = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(186)))), ((int)(((byte)(205)))), ((int)(((byte)(219)))));
+            this.label1.Location = new System.Drawing.Point(99, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Encrypt";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Bahnschrift SemiBold Condensed", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(186)))), ((int)(((byte)(205)))), ((int)(((byte)(219)))));
+            this.label2.Location = new System.Drawing.Point(50, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Select a file:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Bahnschrift SemiBold Condensed", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(186)))), ((int)(((byte)(205)))), ((int)(((byte)(219)))));
+            this.label3.Location = new System.Drawing.Point(229, 264);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 18);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "set an encryption key";
+            // 
+            // txtFileEncrypt
+            // 
+            this.txtFileEncrypt.Location = new System.Drawing.Point(53, 158);
+            this.txtFileEncrypt.Name = "txtFileEncrypt";
+            this.txtFileEncrypt.Size = new System.Drawing.Size(292, 20);
+            this.txtFileEncrypt.TabIndex = 3;
+            // 
+            // txtEncryptPassword
+            // 
+            this.txtEncryptPassword.Location = new System.Drawing.Point(53, 294);
+            this.txtEncryptPassword.Name = "txtEncryptPassword";
+            this.txtEncryptPassword.Size = new System.Drawing.Size(292, 20);
+            this.txtEncryptPassword.TabIndex = 4;
+            this.txtEncryptPassword.TextChanged += new System.EventHandler(this.txtEncryptPassword_TextChanged);
+            // 
+            // btnBrowseEncrypt
+            // 
+            this.btnBrowseEncrypt.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(186)))), ((int)(((byte)(205)))), ((int)(((byte)(219)))));
+            this.btnBrowseEncrypt.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBrowseEncrypt.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
+            this.btnBrowseEncrypt.Location = new System.Drawing.Point(53, 184);
+            this.btnBrowseEncrypt.Name = "btnBrowseEncrypt";
+            this.btnBrowseEncrypt.Size = new System.Drawing.Size(142, 34);
+            this.btnBrowseEncrypt.TabIndex = 5;
+            this.btnBrowseEncrypt.Text = "Browse Files";
+            this.btnBrowseEncrypt.UseVisualStyleBackColor = false;
+            this.btnBrowseEncrypt.Click += new System.EventHandler(this.btnBrowseEncrypt_Click);
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(186)))), ((int)(((byte)(205)))), ((int)(((byte)(219)))));
+            this.btnEncrypt.Enabled = false;
+            this.btnEncrypt.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEncrypt.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
+            this.btnEncrypt.Location = new System.Drawing.Point(53, 320);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(142, 34);
+            this.btnEncrypt.TabIndex = 6;
+            this.btnEncrypt.Text = "Encrypt File";
+            this.btnEncrypt.UseVisualStyleBackColor = false;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(186)))), ((int)(((byte)(205)))), ((int)(((byte)(219)))));
+            this.btnDecrypt.Enabled = false;
+            this.btnDecrypt.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDecrypt.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
+            this.btnDecrypt.Location = new System.Drawing.Point(475, 320);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(142, 34);
+            this.btnDecrypt.TabIndex = 13;
+            this.btnDecrypt.Text = "Decrypt File";
+            this.btnDecrypt.UseVisualStyleBackColor = false;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // btnBrowseDecrypt
+            // 
+            this.btnBrowseDecrypt.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(186)))), ((int)(((byte)(205)))), ((int)(((byte)(219)))));
+            this.btnBrowseDecrypt.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBrowseDecrypt.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
+            this.btnBrowseDecrypt.Location = new System.Drawing.Point(475, 184);
+            this.btnBrowseDecrypt.Name = "btnBrowseDecrypt";
+            this.btnBrowseDecrypt.Size = new System.Drawing.Size(142, 34);
+            this.btnBrowseDecrypt.TabIndex = 12;
+            this.btnBrowseDecrypt.Text = "Browse Files";
+            this.btnBrowseDecrypt.UseVisualStyleBackColor = false;
+            this.btnBrowseDecrypt.Click += new System.EventHandler(this.btnBrowseDecrypt_Click);
+            // 
+            // txtDecryptPassword
+            // 
+            this.txtDecryptPassword.Location = new System.Drawing.Point(475, 294);
+            this.txtDecryptPassword.Name = "txtDecryptPassword";
+            this.txtDecryptPassword.Size = new System.Drawing.Size(292, 20);
+            this.txtDecryptPassword.TabIndex = 11;
+            this.txtDecryptPassword.TextChanged += new System.EventHandler(this.txtDecryptPassword_TextChanged);
+            // 
+            // txtFileDecrypt
+            // 
+            this.txtFileDecrypt.Location = new System.Drawing.Point(475, 158);
+            this.txtFileDecrypt.Name = "txtFileDecrypt";
+            this.txtFileDecrypt.Size = new System.Drawing.Size(292, 20);
+            this.txtFileDecrypt.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Bahnschrift SemiBold Condensed", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(186)))), ((int)(((byte)(205)))), ((int)(((byte)(219)))));
+            this.label4.Location = new System.Drawing.Point(616, 264);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(151, 18);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "confirm your encryption key";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Bahnschrift SemiBold Condensed", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(186)))), ((int)(((byte)(205)))), ((int)(((byte)(219)))));
+            this.label5.Location = new System.Drawing.Point(472, 128);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 18);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Select a file:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(186)))), ((int)(((byte)(205)))), ((int)(((byte)(219)))));
+            this.label6.Location = new System.Drawing.Point(521, 34);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(98, 33);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Decrypt";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(186)))), ((int)(((byte)(205)))), ((int)(((byte)(219)))));
+            this.panel1.Location = new System.Drawing.Point(409, 23);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1, 369);
+            this.panel1.TabIndex = 14;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
+            this.ClientSize = new System.Drawing.Size(816, 418);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.btnBrowseDecrypt);
+            this.Controls.Add(this.txtDecryptPassword);
+            this.Controls.Add(this.txtFileDecrypt);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.btnBrowseEncrypt);
+            this.Controls.Add(this.txtEncryptPassword);
+            this.Controls.Add(this.txtFileEncrypt);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtFileEncrypt;
+        private System.Windows.Forms.TextBox txtEncryptPassword;
+        private System.Windows.Forms.Button btnBrowseEncrypt;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.Button btnBrowseDecrypt;
+        private System.Windows.Forms.TextBox txtDecryptPassword;
+        private System.Windows.Forms.TextBox txtFileDecrypt;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ HiddenDimensions/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HiddenDimensions
+{
+    public partial class Form1 : Form
+    {
+        private string inputFileLocation;
+        private string outputFileLocation;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "Hidden Dimension";
+        }
+
+        public static byte[] EncryptStringToBytes_Aes(string plainText, byte[] key, byte[] iv)
+        {
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = key;
+                aesAlg.IV = iv;
+
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            swEncrypt.Write(plainText);
+                        }
+                        return msEncrypt.ToArray();
+                    }
+                }
+            }
+        }
+
+        public static string DecryptStringFromBytes_Aes(byte[] cipherText, byte[] key, byte[] iv)
+        {
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = key;
+                aesAlg.IV = iv;
+
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+                using (MemoryStream msDecrypt = new MemoryStream(cipherText))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+                            return srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+            }
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(inputFileLocation))
+            {
+                MessageBox.Show("Please select a file to decrypt.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            string password = txtDecryptPassword.Text;
+
+            // Generate a key and IV from the password
+            using (var derivedBytes = new Rfc2898DeriveBytes(password, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 }, 10000))
+            {
+                byte[] key = derivedBytes.GetBytes(32); // AES-256 key size
+                byte[] iv = derivedBytes.GetBytes(16); // AES block size
+
+                // Read the encrypted file
+                byte[] encryptedBytes = File.ReadAllBytes(inputFileLocation);
+
+                // Decrypt the content
+                string decryptedText = DecryptStringFromBytes_Aes(encryptedBytes, key, iv);
+
+                // Display the decrypted content in a new TextBox control
+                var textBox = new TextBox();
+                textBox.Dock = DockStyle.Fill;
+                textBox.Multiline = true;
+                textBox.ReadOnly = true;
+                textBox.BackColor = Color.FromArgb(0x30, 0x30, 0x30);
+                textBox.ForeColor = Color.FromArgb(0xba, 0xcd, 0xdb);
+                textBox.Font = new Font("Palatino Linotype", 10, FontStyle.Bold);
+                textBox.Text = decryptedText;
+                textBox.SelectionStart = 0;
+                textBox.SelectionLength = 0;
+                textBox.Padding = new Padding(10, 0, 0, 0); // add a left padding of 10 pixels
+                textBox.ScrollBars = ScrollBars.Vertical; // enable vertical scrollbar
+
+                var form = new Form();
+                form.Controls.Add(textBox);
+                form.WindowState = FormWindowState.Maximized;
+                form.Show();
+            }
+        }
+
+        private void btnBrowseDecrypt_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Encrypted Files (*.hd)|*.hd|All Files (*.*)|*.*";
+                openFileDialog.Multiselect = false;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    inputFileLocation = openFileDialog.FileName;
+                    txtFileDecrypt.Text = inputFileLocation;
+                }
+            }
+        }
+
+        private void txtDecryptPassword_TextChanged(object sender, EventArgs e)
+        {
+            btnDecrypt.Enabled = !string.IsNullOrEmpty(txtDecryptPassword.Text);
+        }
+
+        private void txtEncryptPassword_TextChanged(object sender, EventArgs e)
+        {
+            btnEncrypt.Enabled = !string.IsNullOrEmpty(txtEncryptPassword.Text);
+        }
+
+        private void btnBrowseEncrypt_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Text Files (*.txt)|*.txt|Word Documents (*.docx)|*.docx|All Files (*.*)|*.*";
+                openFileDialog.Multiselect = false;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    inputFileLocation = openFileDialog.FileName;
+                    txtFileEncrypt.Text = inputFileLocation;
+                }
+            }
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(inputFileLocation))
+            {
+                MessageBox.Show("Please select a file to encrypt.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            string password = txtEncryptPassword.Text;
+
+            // Generate a key and IV from the password
+            using (var derivedBytes = new Rfc2898DeriveBytes(password, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 }, 10000))
+            {
+                byte[] key = derivedBytes.GetBytes(32); // AES-256 key size
+                byte[] iv = derivedBytes.GetBytes(16); // AES block size
+
+                // Read the input file
+                string plainText = File.ReadAllText(inputFileLocation);
+
+                // Encrypt the content
+                byte[] encryptedBytes = EncryptStringToBytes_Aes(plainText, key, iv);
+
+                // Save the encrypted content to a new file
+                using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+                {
+                    saveFileDialog.Filter = "Encrypted Files (*.hd)|*.hd|All Files (*.*)|*.*";
+                    if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        outputFileLocation = saveFileDialog.FileName;
+                        File.WriteAllBytes(outputFileLocation, encryptedBytes);
+                        MessageBox.Show("Encryption completed. Hidden Dimension File saved successfully!.");
+                    }
+                }
+            }
+        }
+    }
+}
